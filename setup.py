3c3183c0efee4f08cf53b3c532b2d71fe7c41681
Hemen Kapadia <hemen.kapadia@gmail.com>
2020-05-21 00:15:43 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,37 @@
-import setuptools
+from setuptools import setup
 
+# read version
+about_info = {}
+with open("version.txt", "r") as v:
+    exec(v.read(), about_info)
+# read long description
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name="layoutlm", # Replace with your own username
-    version="0.0.1",
-    author="Lei Cui",
-    author_email="fuwei@microsoft.com",
-    description="Document entity recognition",
+setup(
+    name="h2o_layoutlm",
+    version=about_info['version'],
+    description="H2O.ai port of Microsoft LayoutLM Document entity recognition",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/microsoft/unilm",
-    #packages=setuptools.find_packages(),
-    packages=['layoutlm'],
+    url="https://www.h2o.ai",
+    author="H2O.ai",
+    author_email="team@h2o.ai",
+    license="MIT License",
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 1 - Planning",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Recognition",
     ],
-    python_requires='>=3.6',
+    keywords="",
+    packages=['layoutlm'],
+    python_requires='~=3.6',
+    install_requires=[],
+    dependency_links=[],
+    # package_data={},
+    # data_files=[],
+    # entry_points={},
 )
